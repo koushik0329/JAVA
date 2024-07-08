ecce2645ab518a1c2cbf67e50cdcafe3687a7439
Koushik Reddy Kambham <102892696+koushik0329@users.noreply.github.com>
2024-07-08 18:05:20 -0500

    Add files via upload

---
+++ Adv/servlet examples/session1/src/main/java/session1/SessionServlet.java
@@ -0,0 +1,43 @@
+package session1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		to get session information
+		PrintWriter pw=response.getWriter();
+		HttpSession hs=request.getSession();
+		String s=hs.getId();
+		pw.println(s);
+		pw.println(new Date(hs.getCreationTime()));
+		pw.println(new Date(hs.getLastAccessedTime()));
+		pw.println(hs.getMaxInactiveInterval());
+	}
+
+}
