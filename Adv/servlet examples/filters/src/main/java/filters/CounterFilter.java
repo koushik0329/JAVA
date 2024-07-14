1280863c499523c70c4d0536cf4c7a7fe0ef63ad
Koushik Reddy Kambham <102892696+koushik0329@users.noreply.github.com>
2024-07-14 14:44:36 -0500

    filters

---
+++ Adv/servlet examples/filters/src/main/java/filters/CounterFilter.java
@@ -0,0 +1,59 @@
+package filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * Servlet Filter implementation class CounterFilter
+ */
+public class CounterFilter extends HttpFilter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+	int count=0;
+    public CounterFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//using httpservletrequest and httpservletrespose because we want to access http methods
+		// TODO Auto-generated method stub
+		// place your code here
+		count++;
+		ServletContext sc=request.getServletContext();
+		sc.setAttribute("views", count);
+		 
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
